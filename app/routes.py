5ee2e6405e747e3dd855835bd1a11633940ee6ed
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-03-07 22:44:11 -0600

    jinja

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'name': 'Din'}
+    n = 10
+    flag = True
+    return render_template(
+        'index.html', 
+        title='TExp', 
+        username=user['name'], 
+        flag=flag, 
+        n=n
+        )
